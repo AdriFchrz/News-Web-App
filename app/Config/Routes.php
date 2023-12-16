1be94dfe3868f0b9cf3dcb2dbc2c349d0ae8cfe7
314dhan <ramadhan25321@gmail.com>
2023-12-16 19:28:04 +0700

    update routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,5 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'HomeController::index');
 $routes->get('/news/detail/(:num)', 'NewsController::detail/$1');
+$routes->get('/news/by_category/(:num)', 'HomeController::newsByCategory/$1');
+$routes->post('/search', 'HomeController::search');
