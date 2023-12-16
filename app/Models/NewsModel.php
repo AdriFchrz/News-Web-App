92e8cf87c9e48dfeb1b5cbdcdee2d1dcc7f6f424
314dhan <ramadhan25321@gmail.com>
2023-12-16 19:30:13 +0700

    update NewsModel.php with some create some function: getNewsWithAuthorAndCategory, getNewsByCateogry, searchNewsByTitle, getInstance

--- app/Models/NewsModel.php
+++ app/Models/NewsModel.php
@@ -11,6 +11,8 @@ class NewsModel extends Model
     protected $primaryKey = 'id';
     protected $allowedFields = ['title', 'content', 'category_id', 'author_id', 'created_at', 'updated_at'];
 
+    protected static $instance;
+
     public function getAllNews()
     {
         return $this->findAll();
@@ -24,6 +26,27 @@ class NewsModel extends Model
             ->first();
     }
 
+    public function getNewsByCategory($category_id)
+    {
+        return $this->where('category_id', $category_id)->findAll();
+    }
+
+    // NewsModel.php
+
+    public function searchNewsByTitle($keyword)
+    {
+        return $this->like('title', $keyword)->findAll();
+    }
+
+
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new static();
+        }
+
+        return self::$instance;
+    }
 
 
 }
