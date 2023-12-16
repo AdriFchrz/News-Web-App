bf1b11e8cdf246d14385a0471f1b228e1165a8de
314dhan <ramadhan25321@gmail.com>
2023-12-16 19:27:27 +0700

    update HomeController with search, category, and singleton object

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,11 +6,40 @@ use App\Models\NewsModel;
 
 class HomeController extends BaseController
 {
+    protected $newsModel;
+
+    public function __construct()
+    {
+        $this->newsModel = NewsModel::getInstance();
+    }
+
     public function index()
     {
-        $newsModel = new NewsModel();
-        $data['news'] = $newsModel->getAllNews();
+        $data['news'] = $this->newsModel->getAllNews();
         echo view('layout/header');
         echo view('home', $data);
     }
+
+    public function newsByCategory($category_id)
+    {
+        $data['news'] = $this->newsModel->getNewsByCategory($category_id);
+
+        echo view('layout/header');
+        echo view('category', $data);
+    }
+
+    public function search()
+    {
+        $keyword = $this->request->getVar('search');
+        $data['news'] = $this->newsModel->searchNewsByTitle($keyword);
+
+        echo view('layout/header');
+
+        if (!empty($data['news'])) {
+            echo view('search', $data);
+        } else {
+            echo view('not_found');
+        }
+    }
 }
+
